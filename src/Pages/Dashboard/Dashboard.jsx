e974e45ccbff3be590d7a007fdc1b06b356711d5
sikander-afzaal <sikander.aaa@gmail.com>
2022-11-22 21:29:20 +0500

    asdasd

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -2,6 +2,38 @@ import OverviewCard from "../../Components/OverviewCards/OverviewCard";
 import "./Dashboard.css";
 
 const Dashboard = () => {
+  const tableData = [
+    {
+      img: "/assets/user1.png",
+      name: "Erika Bartell",
+      msg: "Hi how are you how are. Can we talk now? Ping Me Back...",
+      lastSeen: "20 April 2016",
+      firstSeen: "20 May 2016",
+      total: "3",
+      min: "2",
+      max: "5",
+    },
+    {
+      img: "/assets/user1.png",
+      name: "Erika Bartell",
+      msg: "Hi how are you how are. Can we talk now? Ping Me Back...",
+      lastSeen: "20 April 2016",
+      firstSeen: "20 May 2016",
+      total: "3",
+      min: "2",
+      max: "5",
+    },
+    {
+      img: "/assets/user1.png",
+      name: "Erika Bartell",
+      msg: "Hi how are you how are. Can we talk now? Ping Me Back...",
+      lastSeen: "20 April 2016",
+      firstSeen: "20 May 2016",
+      total: "3",
+      min: "2",
+      max: "5",
+    },
+  ];
   return (
     <div className="dashboard">
       <div className="top-dashboard">
@@ -48,8 +80,52 @@ const Dashboard = () => {
           percent="1.30%"
         />
       </div>
+      <div className="conversation-table">
+        <div className="top-table">
+          <h2>Conversations</h2>
+          <div className="filter-div">
+            <img src="/assets/ico-filter.png" alt="" />
+            <p>Filter</p>
+          </div>
+        </div>
+        <div className="bottom-table-wrap">
+          <div className="bottom-table">
+            <div className="table-head">
+              <h2 className="left-align">User Name And Messages</h2>
+              <h2>First Seen Date</h2>
+              <h2>Last Seen Date</h2>
+              <h2>Total Conversations</h2>
+              <h2>Min Conv. Steps</h2>
+              <h2>Max Conv. Steps</h2>
+            </div>
+
+            {tableData.map((elem, idx) => {
+              return <TableRow {...elem} key={"table-row" + idx} />;
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Dashboard;
+
+const TableRow = ({ img, name, msg, lastSeen, firstSeen, total, min, max }) => {
+  return (
+    <div className="table-row">
+      <div className="first-col">
+        <img src={img} alt="" />
+        <div>
+          <h3>{name}</h3>
+          <p>{msg}</p>
+        </div>
+      </div>
+      <div>{firstSeen}</div>
+      <div>{lastSeen}</div>
+      <div>{total}</div>
+      <div>{min}</div>
+      <div>{max}</div>
+    </div>
+  );
+};
